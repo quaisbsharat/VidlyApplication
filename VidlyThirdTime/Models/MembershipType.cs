ac204a6760521418e0eedc3581c96063b71ec4e2
QaisBsharat <kaisbsharat@itc.hu.edu.jo>
2017-05-22 16:26:17 +0300

    Add another Comment for test githup

--- VidlyThirdTime/Models/MembershipType.cs
+++ VidlyThirdTime/Models/MembershipType.cs
@@ -2,6 +2,7 @@
 {
     public class MembershipType
     {
+        //This is a Test for githup
         public byte Id { get; set; }
         public short SignUpFee { get; set; } // سعر التسجيل في العضويه
         public byte DurationInMonth { get; set; }
