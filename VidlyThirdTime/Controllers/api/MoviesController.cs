255e3bcd7ab2c6a97b92459cf10140873b2ebc6e
QaisBsharat <kaisbsharat@itc.hu.edu.jo>
2017-05-26 19:02:55 +0300

    Buliding API

---
+++ VidlyThirdTime/Controllers/api/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using VidlyThirdTime.Models;
+
+namespace VidlyThirdTime.Controllers.api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+    }
+}
